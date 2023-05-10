bdb5882ad26e1631525f8711c0c09d4d57a1c45c
Yccc2333 <yxcakanen@gmail.com>
2023-05-10 14:25:17 +0200

    MD_2_HOMEWORK

---
+++ Quarto and MD 2/Rmd2Quarto.R
@@ -0,0 +1,3 @@
+library(knitr)
+setwd('/Users/huyiqing/PycharmProjects/UW_lab/RRcourse2023/Quarto and MD 2/Homework')
+knitr::convert_chunk_header(input = "Assignment.Rmd", output = "Assignment.qmd")
